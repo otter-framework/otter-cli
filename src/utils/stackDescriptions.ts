fa1f10d6b05fd9633e851611ff46e6a2f896b2c3
Chris <chris@solaris.lan>
2023-03-14 15:15:32 -0400

    finishing touches

--- src/utils/stackDescriptions.ts
+++ src/utils/stackDescriptions.ts
@@ -34,4 +34,4 @@ export const turnStack: StackDescription = {
   deployCompleteMessage: "Otter STUN/TURN cluster deployed",
 };
 
-export const stacks: StackDescription[] = [signalStack, apiStack, turnStack];
+export const stacks: StackDescription[] = [turnStack];
